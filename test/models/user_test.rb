9378b850c8c0d832799c04a0701c47f93997ad82
CyrilPerrinjaquet <cyrilp.perso@gmail.com>
2023-08-11 08:20:10 +0200

    :sparkles: Created User model

---
+++ test/models/user_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test "user with a valid email should be valid" do
+    user = User.new(email: "test@test.org", password_digest: 'test')
+    assert user.valid?
+  end
+
+  test "user with unvalid email shouldn't be valid" do
+    user = User.new(email: "test.org", password_digest: "test")
+    assert_not user.valid?
+  end
+
+  test "user with not unique email shouldn't be valid" do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest: "test")
+    assert_not user.valid?
+  end
+end
