c97a2f096d6e3be4d8ad5e5eb2d7baf738977c09
CyrilPerrinjaquet <cyrilp.perso@gmail.com>
2023-10-06 16:29:21 +0200

    :sparkles: Added the create method for the orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
       resources :products
-      resources :orders, only: [:index]
+      resources :orders, only: %i[index show create]
     end
   end
 end
