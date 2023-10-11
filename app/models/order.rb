a9513a2bd39a9e70d41e9afc66ae3b7917c3c162
CyrilPerrinjaquet <cyrilp.perso@gmail.com>
2023-10-11 13:21:52 +0200

    :white_check_mark: Updates the total calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ApplicationRecord
+  include ActiveModel::Validations
   belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
   before_validation :set_total!
+  validates_with EnoughProductsValidator
 
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantites)
     product_ids_and_quantites.each do |product_id_and_quantity|
@@ -15,6 +17,6 @@ class Order < ApplicationRecord
   end
 
   def set_total!
-    self.total = self.products.map(&:price).sum
+    self.total = self.placements.map { |placement| placement.product.price * placement.quantity }.sum
   end
 end
